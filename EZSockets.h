98701e8fb6d96a8b13bae17c2c6fd1f64f888123
Cootkri <animooshing@gmail.com>
2016-04-10 21:23:51 -0400

    Added files via upload

---
+++ EZSockets.h
@@ -0,0 +1,51 @@
+#ifndef EZSOCKETS_H
+#define EZSOCKETS_H
+
+#include <WinSock2.h>
+#include <Windows.h>
+#include <string>
+#include <iostream>
+
+enum Packet{
+	Packet_ERROR,
+	Packet_PING,
+	Packet_String,
+	Packet_Int
+};
+
+void InitWSA();
+HANDLE SetCallback(void(*f));
+HANDLE SetCallback(void(*f)(int), int param);
+
+void MergeInts(char* dest, Packet pack, int value);
+void DivergeInts(char* src, Packet *pack, int *value);
+
+class EZSocket{
+public:
+	EZSocket();
+	~EZSocket();
+
+	SOCKET* GetSocket(){ return &mySoc; }
+	SOCKADDR_IN* GetPartner(){ return &partner; }
+	void SetSocket(SOCKET soc){ mySoc = soc; }
+	void SetPartner(SOCKADDR_IN part){ partner = part; }
+
+	bool Connect(std::string ip, int port);
+	void CloseConnection();
+	void Listen(std::string ip, int port);
+	SOCKET Accept();
+
+	bool SendString(const char* message, int length);
+	bool SendString(std::string msg);
+	bool SendInt(int num);
+	bool SendPing();
+
+	void ReceivePacket(Packet *pack, int *value);
+	void ReceiveData(char* data, int length);
+	std::string ReceiveString(int length);
+private:
+	SOCKET mySoc;
+	SOCKADDR_IN partner;
+};
+
+#endif
